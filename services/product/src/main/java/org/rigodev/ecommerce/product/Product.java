eb60014308ca8684554c85c305eb99f20a9a65f7
The-Rigo <rodri.ledezma9483@gmail.com>
2026-03-04 23:21:29 -0400

    Create the entity and init the database

---
+++ services/product/src/main/java/org/rigodev/ecommerce/product/Product.java
@@ -0,0 +1,35 @@
+package org.rigodev.ecommerce.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.rigodev.ecommerce.category.Category;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private Double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
