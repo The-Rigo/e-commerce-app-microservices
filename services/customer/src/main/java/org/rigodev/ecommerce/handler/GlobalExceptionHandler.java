3ee48b40494e42dfea5de7e19ed53b3e6a1adec4
The-Rigo <rodri.ledezma9483@gmail.com>
2026-03-10 23:38:57 -0400

    Exception Handling & insert test data

--- services/customer/src/main/java/org/rigodev/ecommerce/handler/GlobalExceptionHandler.java
+++ services/customer/src/main/java/org/rigodev/ecommerce/handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package org.rigodev.ecommerce;
+package org.rigodev.ecommerce.handler;
 
 import org.rigodev.ecommerce.exception.CustomerNotFoundException;
-import org.rigodev.ecommerce.handler.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
